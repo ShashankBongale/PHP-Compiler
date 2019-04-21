5269b15151e42af8f5cf93b53dba316fa98ac09a
shashankbongale <shashankbongale.com>
2019-04-21 10:18:14 +0530

    removed in master

--- README.md
+++
@@ -1,7 +0,0 @@
-# php_compiler
-A simple compiler for php
-### compilation steps
-1)lex php_lex.l
-2)yacc php_yacc.y
-3)gcc lex.yyc y.tab.c -o run
-4)./run
--- symbol_table.h
+++
@@ -1,8 +0,0 @@
-typedef struct symbol_table
-{
-  int line;
-  char name[31];
-}ST;
-void lookup(char *,int );
-void insert(char *,int );
-void search_id(char *,int );
